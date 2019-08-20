f4d3b84fbc9607d3b1773e24d038dea41aea7f7d
arun-sraj <arunraj@stayntouch.com>
2019-08-20 18:07:33 +0530

    Adding more options

---
+++ src/jobs/Staging.groovy
@@ -0,0 +1,445 @@
+// Main job
+job("aws-account-setup-staging") {
+    description()
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+//chef setup
+
+folder("chef-setup-staging") {
+    description 'This job will replace all template instances with the latest Chef configuration.  We will need one job per env & application type.  The steps it will follow for each template type are: \
+    Start new template instance \
+    Bootstrap it with Chef Server \
+    Execute chef client to configure node (with startup scripts disabled / commented out) \
+    Shutdown template instance \
+    Take template image \
+    Terminate template instance'
+    views {
+        listView('auth') {
+            description('Auth chef setup jobs')
+            filterBuildQueue()
+            filterExecutors()
+            jobs {
+                name('auth-staging')
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+                lastDuration()
+                buildButton()
+            }
+        }
+        listView('general service') {
+            description('All general service jobs')
+            filterBuildQueue()
+            filterExecutors()
+            jobs {
+                names('postfix-staging', 'mq-app-staging', 'mq-staging', 'glusterfs-staging', ' agent-staging')
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+                lastDuration()
+                buildButton()
+            }
+        }
+        listView('ifc') {
+            description('ifc chef setup jobs')
+            filterBuildQueue()
+            filterExecutors()
+            jobs {
+                name('ifc-staging')
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+                lastDuration()
+                buildButton()
+            }
+        }
+        listView('pms') {
+            description('pms chef setup jobs')
+            filterBuildQueue()
+            filterExecutors()
+            jobs {
+                name('pms-staging')
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+                lastDuration()
+                buildButton()
+            }
+        }
+        listView('webhook') {
+            description('webhook chef setup jobs')
+            filterBuildQueue()
+            filterExecutors()
+            jobs {
+                name('webhook-staging')
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+                lastDuration()
+                buildButton()
+            }
+        }
+    }
+}
+job("agent-staging") {
+    description("Job for deploying agent service for the staging environment.")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("auth-staging") {
+    description("Deploy Chef cookbooks for staging environment using")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("glusterfs-staging") {
+    description("Job for deploying glusterfs service for the staging environment.")
+    keepDependencies(false)
+    disabled(true)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("ifc-staging") {
+    description("Deploy Chef cookbooks of staging environment for ifc servers")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("mq-app-staging") {
+    description("Deploy Chef cookbooks for staging environment using")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("mq-staging") {
+    description("Job for deploying mq service for the staging environment.")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("pms-staging") {
+    description("Deploy Chef cookbooks for staging environment using")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("postfix-staging") {
+    description("Job for deploying postfix service for the staging environment.")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
+
+job("webhook-staging") {
+    description("Deploy Chef cookbooks for staging environment using")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    steps {
+        downstreamParameterized {
+            trigger("") {
+                block {
+                    buildStepFailure("FAILURE")
+                    unstable("UNSTABLE")
+                    failure("FAILURE")
+                }
+            }
+        }
+    }
+    publishers {
+        mailer("devops@stayntouch.com", false, true)
+    }
+    configure {
+        it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+            strategy {
+                'daysToKeep'('3')
+                'numToKeep'('-1')
+                'artifactDaysToKeep'('-1')
+                'artifactNumToKeep'('-1')
+            }
+        }
+        it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+            'autoRebuild'('false')
+            'rebuildDisabled'('false')
+        }
+    }
+}
