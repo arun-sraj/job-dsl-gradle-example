43db5d9761a27653d7349e4a53468e0dc672e07a
arun-sraj <arunraj@stayntouch.com>
2019-08-21 17:50:01 +0530

    test

--- src/jobs/ReleaseJobs.groovy
+++ src/jobs/ReleaseJobs.groovy
@@ -1,7 +1,7 @@
 String currentDir = new File(".").getAbsolutePath();
 println "${currentDir}"
 
-evaluate(readFileFromWorkspace("src/jobs/Templates.groovy"))
+evaluate(readFileFromWorkspace("src/jobs/Templates.groovy").toString())
 
 def template = new Templates()
 template.ConfigureJobs("release")
