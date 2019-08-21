5f76f915722708c39fd2a91d2bc328abe960a1b7
arun-sraj <arunraj@stayntouch.com>
2019-08-21 15:57:03 +0530

    file path

--- src/jobs/ReleaseJobs.groovy
+++ src/jobs/ReleaseJobs.groovy
@@ -1,5 +1,5 @@
 
-evaluate(new File(System.getenv('WORKSPACE')+"/src/jobs/Templates.groovy"))
+evaluate(new File(build.getBuildVariables().get('WORKSPACE')+"/src/jobs/Templates.groovy"))
 
 def template = new Templates()
 template.ConfigureJobs("release")
