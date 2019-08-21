454c08c3c8a6c41296eb6d27dcf68e91a5208196
arun-sraj <arunraj@stayntouch.com>
2019-08-21 17:44:36 +0530

    test

--- src/jobs/ReleaseJobs.groovy
+++ src/jobs/ReleaseJobs.groovy
@@ -1,13 +1,7 @@
 String currentDir = new File(".").getAbsolutePath();
 println "${currentDir}"
 
-def sout = new StringBuilder(), serr = new StringBuilder()
-def proc = 'ls'.execute()
-proc.consumeProcessOutput(sout, serr)
-proc.waitForOrKill(1000)
-println "out> $sout err> $serr"
-
-evaluate(new File("Templates.groovy"))
+evaluate(readFileFromWorkspace("src/jobs/Templates.groovy"))
 
 def template = new Templates()
 template.ConfigureJobs("release")
