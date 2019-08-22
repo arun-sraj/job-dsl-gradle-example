691fb70b92900b855ef8f6ede5a7625cf761a5df
arun-sraj <arunraj@stayntouch.com>
2019-08-22 13:07:48 +0530

    Job DSL configuration

--- src/jobs/ReleaseJobs.groovy
+++ src/jobs/ReleaseJobs.groovy
@@ -3,6 +3,9 @@ import utilities.Templates
 
 String environment = 'release'
 
-//Account setup job
+// Account setup job
 def awsAccountSetupJob = job("aws-account-setup-$environment")
 Templates.awsAccountSetup(awsAccountSetupJob, "$environment")
+// Chef Folder setup
+def chefFolder = folder("chef-setup-$environment")
+Templates.chefFolderSetup(chefFolder, "$environment")
--- src/jobs/utilities/Templates.groovy
+++ src/jobs/utilities/Templates.groovy
@@ -1,175 +1,141 @@
 package utilities
 class Templates {
   static void awsAccountSetup(def job, String environment) {
-      job.with {
-          description("Arbitrary feature $environment")
-          keepDependencies(false)
-          disabled(false)
-          concurrentBuild(false)
-          steps {
-              downstreamParameterized {
-                  trigger("") {
-                      block {
-                          buildStepFailure("FAILURE")
-                          unstable("UNSTABLE")
-                          failure("FAILURE")
-                      }
+    job.with {
+      description("Account serup job for environment $environment")
+      keepDependencies(false)
+      disabled(false)
+      concurrentBuild(false)
+      steps {
+          downstreamParameterized {
+              trigger("./aws-account-setup") {
+                  block {
+                      buildStepFailure("FAILURE")
+                      unstable("UNSTABLE")
+                      failure("FAILURE")
                   }
-              }
-          }
-          publishers {
-              mailer("devops@stayntouch.com", false, true)
-          }
-          configure {
-              it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
-                  strategy {
-                      'daysToKeep'('3')
-                      'numToKeep'('-1')
-                      'artifactDaysToKeep'('-1')
-                      'artifactNumToKeep'('-1')
-                  }
-              }
-              it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
-                  'autoRebuild'('false')
-                  'rebuildDisabled'('false')
               }
           }
       }
+      publishers {
+          mailer("devops@stayntouch.com", false, true)
+      }
+      configure {
+          it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+              strategy {
+                  'daysToKeep'('3')
+                  'numToKeep'('-1')
+                  'artifactDaysToKeep'('-1')
+                  'artifactNumToKeep'('-1')
+              }
+          }
+          it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+              'autoRebuild'('false')
+              'rebuildDisabled'('false')
+          }
+      }
+    }
   }
+
+    //chef setup
+  static void awsAccountSetup(def folder, String environment) {
+    folder.with {
+      description 'This job will replace all template instances with the latest Chef configuration.  We will need one job per env & application type.  The steps it will follow for each template type are: \
+      Start new template instance \
+      Bootstrap it with Chef Server \
+      Execute chef client to configure node (with startup scripts disabled / commented out) \
+      Shutdown template instance \
+      Take template image \
+      Terminate template instance'
+      views {
+          listView('auth') {
+              description('Auth chef setup jobs')
+              filterBuildQueue()
+              filterExecutors()
+              jobs {
+                  name('auth-${environment}')
+              }
+              columns {
+                  status()
+                  weather()
+                  name()
+                  lastSuccess()
+                  lastFailure()
+                  lastDuration()
+                  buildButton()
+              }
+          }
+          listView('general service') {
+              description('All general service jobs')
+              filterBuildQueue()
+              filterExecutors()
+              jobs {
+                  names('postfix-${environment}', 'mq-app-${environment}', 'mq-${environment}', 'glusterfs-${environment}', ' agent-${environment}')
+              }
+              columns {
+                  status()
+                  weather()
+                  name()
+                  lastSuccess()
+                  lastFailure()
+                  lastDuration()
+                  buildButton()
+              }
+          }
+          listView('ifc') {
+              description('ifc chef setup jobs')
+              filterBuildQueue()
+              filterExecutors()
+              jobs {
+                  name('ifc-${environment}')
+              }
+              columns {
+                  status()
+                  weather()
+                  name()
+                  lastSuccess()
+                  lastFailure()
+                  lastDuration()
+                  buildButton()
+              }
+          }
+          listView('pms') {
+              description('pms chef setup jobs')
+              filterBuildQueue()
+              filterExecutors()
+              jobs {
+                  name('pms-${environment}')
+              }
+              columns {
+                  status()
+                  weather()
+                  name()
+                  lastSuccess()
+                  lastFailure()
+                  lastDuration()
+                  buildButton()
+              }
+          }
+          listView('webhook') {
+              description('webhook chef setup jobs')
+              filterBuildQueue()
+              filterExecutors()
+              jobs {
+                  name('webhook-${environment}')
+              }
+              columns {
+                  status()
+                  weather()
+                  name()
+                  lastSuccess()
+                  lastFailure()
+                  lastDuration()
+                  buildButton()
+              }
+          }
+      }
+    }
   }
-    // job("aws-account-setup-${environment}") {
-    //     description()
-    //     keepDependencies(false)
-    //     disabled(false)
-    //     concurrentBuild(false)
-    //     steps {
-    //         downstreamParameterized {
-    //             trigger("") {
-    //                 block {
-    //                     buildStepFailure("FAILURE")
-    //                     unstable("UNSTABLE")
-    //                     failure("FAILURE")
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     publishers {
-    //         mailer("devops@stayntouch.com", false, true)
-    //     }
-    //     configure {
-    //         it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
-    //             strategy {
-    //                 'daysToKeep'('3')
-    //                 'numToKeep'('-1')
-    //                 'artifactDaysToKeep'('-1')
-    //                 'artifactNumToKeep'('-1')
-    //             }
-    //         }
-    //         it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
-    //             'autoRebuild'('false')
-    //             'rebuildDisabled'('false')
-    //         }
-    //     }
-    // }
-
-    // //chef setup
-
-    // folder("chef-setup-${environment}") {
-    //     description 'This job will replace all template instances with the latest Chef configuration.  We will need one job per env & application type.  The steps it will follow for each template type are: \
-    //     Start new template instance \
-    //     Bootstrap it with Chef Server \
-    //     Execute chef client to configure node (with startup scripts disabled / commented out) \
-    //     Shutdown template instance \
-    //     Take template image \
-    //     Terminate template instance'
-    //     views {
-    //         listView('auth') {
-    //             description('Auth chef setup jobs')
-    //             filterBuildQueue()
-    //             filterExecutors()
-    //             jobs {
-    //                 name('auth-${environment}')
-    //             }
-    //             columns {
-    //                 status()
-    //                 weather()
-    //                 name()
-    //                 lastSuccess()
-    //                 lastFailure()
-    //                 lastDuration()
-    //                 buildButton()
-    //             }
-    //         }
-    //         listView('general service') {
-    //             description('All general service jobs')
-    //             filterBuildQueue()
-    //             filterExecutors()
-    //             jobs {
-    //                 names('postfix-${environment}', 'mq-app-${environment}', 'mq-${environment}', 'glusterfs-${environment}', ' agent-${environment}')
-    //             }
-    //             columns {
-    //                 status()
-    //                 weather()
-    //                 name()
-    //                 lastSuccess()
-    //                 lastFailure()
-    //                 lastDuration()
-    //                 buildButton()
-    //             }
-    //         }
-    //         listView('ifc') {
-    //             description('ifc chef setup jobs')
-    //             filterBuildQueue()
-    //             filterExecutors()
-    //             jobs {
-    //                 name('ifc-${environment}')
-    //             }
-    //             columns {
-    //                 status()
-    //                 weather()
-    //                 name()
-    //                 lastSuccess()
-    //                 lastFailure()
-    //                 lastDuration()
-    //                 buildButton()
-    //             }
-    //         }
-    //         listView('pms') {
-    //             description('pms chef setup jobs')
-    //             filterBuildQueue()
-    //             filterExecutors()
-    //             jobs {
-    //                 name('pms-${environment}')
-    //             }
-    //             columns {
-    //                 status()
-    //                 weather()
-    //                 name()
-    //                 lastSuccess()
-    //                 lastFailure()
-    //                 lastDuration()
-    //                 buildButton()
-    //             }
-    //         }
-    //         listView('webhook') {
-    //             description('webhook chef setup jobs')
-    //             filterBuildQueue()
-    //             filterExecutors()
-    //             jobs {
-    //                 name('webhook-${environment}')
-    //             }
-    //             columns {
-    //                 status()
-    //                 weather()
-    //                 name()
-    //                 lastSuccess()
-    //                 lastFailure()
-    //                 lastDuration()
-    //                 buildButton()
-    //             }
-    //         }
-    //     }
-    // }
     // job("chef-setup-${environment}/agent-${environment}") {
     //     description("Job for deploying agent service for the staging environment.")
     //     keepDependencies(false)
