dbf7d292d7f871a07e14c0c2a0cec057d754cd19
arun-sraj <arunraj@stayntouch.com>
2019-09-04 19:10:06 +0530

    Job DSL configuration

--- src/jobs/clientJobs.groovy
+++ src/jobs/clientJobs.groovy
@@ -231,7 +231,7 @@ for (environment in environmentlist) {
       filterBuildQueue()
       filterExecutors()
       jobs {
-          names("aws-account-setup-$environment.key", "chef-setup-$environment.key", "deploy-$environment.key", "infrastructure-setup-$environment.key", "rake-task-$environment.key", "restart-services-$environment.key", "secrets-manager-setup-$environment.key", "utilities-$environment.key")
+          names("aws-account-setup-$environment.key", "chef-setup-$environment.key", "deploy-$environment.key", "infrastructure-setup-$environment.key", "rake-task-$environment.key", "restart-services-$environment.key", "secrets-manager-setup-$environment.key", "utilities-$environment.key", "custvpn-$environment.key")
       }
       columns {
           status()
